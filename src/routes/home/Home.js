b663eb0476921633c68819ed427ec274dc9eafdb
edgarallanglez <edgarallanglez@gmail.com>
2017-05-26 15:57:44 -0600

    Home detected

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -11,6 +11,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
+import asset1 from './mentor.jpg';
+import asset2 from './mentor2.jpg';
 
 class Home extends React.Component {
   static propTypes = {
@@ -25,7 +27,19 @@ class Home extends React.Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <h1 className={s.youth}>Filosofía</h1>
+          <h1 className={s.wild}>Filosofía</h1>
+          <p className={s.philosophy}>
+            Vacía tu mente, piensa sin forma, moldeable, como el agua. Si pones
+             agua en una taza, se convierte en la taza, si pones agua en una
+             botella se convierte en la botella, si la pones en una tetera se
+              convierte en la tetera. El agua puede fluir o puede aplastar.
+               Se como el agua." Esperamos ofrecer una ventana a la realidad del
+                desarrollo tecnológico en la industria, es decir, poner en
+                 practica de forma divertida lo que implica realizar un proyecto
+                  real y lo que conlleva</p>
+          <div className={s.assetContainer}>
+            <img src={asset1} className={s.asset} alt="assets" />
+          </div>
         </div>
       </div>
     );
